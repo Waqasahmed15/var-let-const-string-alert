450529f57451484d8b77a9b7f107924e6d6cc679
unknown <waqasahmedwikki15@gmail.com>
2023-09-22 20:55:39 +0500

    alert,var,let,const practice

---
+++ app.js
@@ -0,0 +1,80 @@
+        //    assigment 1//
+           // 1st//
+
+// Welcom alert //
+// alert("Welcom To The JavaScript Code")//
+
+                //2nd//
+
+
+//Write a script to display following message on your web page:
+alert("Error! Pleas enter a valid password")
+
+                // 3rd//
+
+alert("Welcom To Js Land...\nHappy Coding")
+
+                //4th//
+
+alert("Welcom To Js Land")
+alert("Prevent this page from creating additional dailogs")
+
+                //5th//
+            
+let firstName = ("waqas ")
+let laststName = ("ahmed")
+console.log(firstName + laststName);
+
+        //    assigment 2//
+        
+                // 1st//
+
+
+let username = (5*2)
+alert(username)
+
+                //2nd//
+
+let myName = ("waqas ahmed")
+alert(myName)
+
+
+
+                // 3rd//
+let massage = ("Helo World")
+alert(massage)
+
+
+               // 4th//
+
+const studentname = ("Waqas Ahmed")
+alert(studentname)
+
+let studentage = ("my age is 29")
+alert(studentage)
+
+let studentpation = ("future mern stack develpor ان شاءاللہ")
+alert(studentpation)
+
+               // 5th//
+
+
+let pizza = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+alert(pizza)
+
+
+               // 6th//
+
+let email = ("my email id is waqasmadani9226@gmail.com")
+alert(email)
+
+               // 7th//
+
+let book = (" “A smarter way to learn JavaScript” ")
+book = ("i am tring to learn from the Book a smarter way to learn JavaScript")
+alert(book)
+
+               // 8th//
+
+let displayinhtml = ("yeh! i can write HTML content through JavaScript")
+document.write(displayinhtml)
